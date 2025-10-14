e0d0d309af2d0db44debc4cbdb0de13d34b5743a
Lean Kurt <leankurtescano@gmail.com>
2025-10-14 22:47:44 +0800

    Initialize project

---
+++ etaas-user/app/(auth)/emailSent.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const emailSent = () => {
+  return (
+    <View>
+      <Text>emailSent</Text>
+    </View>
+  )
+}
+
+export default emailSent
---
+++ etaas-user/config/firebaseConfig.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; 
+
+const apiKey = process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+const authDomain = process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.EXPO_PUBLIC_FIREBASE_APP_ID;
+const measurementId = process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID;
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+export { app, db };
